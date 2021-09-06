16e2821b1d63268d9d794deef1500919a563c61a
hawkofsky <hawkofsky112@outlook.com>
2021-09-06 10:11:39 +0800

    add migration directory to .gitignore

---
+++ migrations/2_vault_migration.js
@@ -0,0 +1,27 @@
+// const XVault = artifacts.require("XVault");
+// const Strategy = artifacts.require("Strategy");
+
+const usdtAddress = '0x55d398326f99059ff775485246999027b3197955';
+const vUsdtAddress = '0xfd5840cd36d94d7229439859c0112a4185bc0255';
+const adminAddress = '0xefdca55e4bce6c1d535cb2d0687b5567eef2ae83';
+
+// async function doDeploy(deployer) {
+//   await deployer.deploy(XVault, usdtAddress, adminAddress, adminAddress);
+//   xVaultContract = await XVault.deployed();
+//   await deployer.deploy(Strategy, xVaultContract.address, vUsdtAddress);
+// }
+
+// module.exports = function (deployer) {
+//   deployer.then(async () => {
+//     await doDeploy(deployer);
+//   });
+// };
+
+const XVault = artifacts.require("XVault");
+const Strategy = artifacts.require("Strategy");
+
+module.exports = function (deployer) {
+    deployer.deploy(XVault, usdtAddress, adminAddress, adminAddress).then(function () {
+      return deployer.deploy(Strategy, XVault.address, vUsdtAddress)
+    });
+};
