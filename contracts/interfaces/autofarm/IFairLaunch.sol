8c3c07a5dd95008ea36c6cc5fa7866fd4870fee2
hawkofsky <hawkofsky112@gmail.com>
2022-04-04 10:21:31 +0800

    base-strategy

--- contracts/interfaces/autofarm/IFairLaunch.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
-
-interface IFairLaunch {
-  function deposit(address user, uint256 pid, uint256 amount) external;
-  function withdraw(address user, uint256 pid, uint256 amount) external;
-  function harvest(uint256 pid) external;
-}
